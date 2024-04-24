5d34e8834853be4d68b60f7727ec89bdd4523c09
Galih Laras Prakoso <galih.laras@pass3.id>
2024-04-24 14:45:44 +0700

    finished product service

--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -7,19 +7,30 @@ import Product from '../models/Product';
   providedIn: 'root'
 })
 export class ProductService {
-
+  private isProductsLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private isProductsLoading$: Observable<boolean> = this.isProductsLoading.asObservable()
   private products: BehaviorSubject<Product[]> = new BehaviorSubject([] as Product[])
   private products$: Observable<Product[]> = this.products.asObservable()
 
   constructor(private http: HttpClient) {}
 
+  getProducts() {
+    return this.products$
+  }
+
+  getIsProductsLoading() {
+    return this.isProductsLoading$
+  }
+
   fetchProducts() {
+    this.isProductsLoading.next(true)
     this.http.get<{products: Product[]}>(
       'https://dummyjson.com/products'
     ).pipe(
       map((data) => data.products)
     ).subscribe(data => {
       this.products.next(data)
+      this.isProductsLoading.next(false)
     })
   }
 }
