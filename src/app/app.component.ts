9fa55734a421f1e27c672c73cfdb2f73c8609c45
Galih Laras Prakoso <galih.laras@pass3.id>
2024-04-24 13:38:51 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-crud';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
