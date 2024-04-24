24da6739c123f0592b70e1cb1033c60aea04877b
Galih Laras Prakoso <galih.laras@pass3.id>
2024-04-24 14:06:36 +0700

    add redirect to products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
     path: 'products',
     loadChildren: () =>
       import('./products/products.module')
       .then(m => m.ProductsModule),
-  }
+  },
+
 ];
