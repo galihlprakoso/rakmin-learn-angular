0967cd3ca0a6b718a9d12f2b14b0d66a34d29f10
Galih Laras Prakoso <galih.laras@pass3.id>
2024-04-24 14:36:39 +0700

    add product service

--- src/app/products/services/product.service.spec.ts
+++ src/app/products/services/product.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProductServiceService } from './product-service.service';
+import { ProductServiceService } from './product.service';
 
 describe('ProductServiceService', () => {
   let service: ProductServiceService;
